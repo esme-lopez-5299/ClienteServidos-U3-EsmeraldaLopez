ec126e15b82659916f3ffd1e9df1cb1b3515148f
esme-lopez-r2devpros <esmeralda.lopez@rvdevpros.com>
2020-10-15 02:02:01 -0500

    Piedra Papel o Tijera Clase 1

---
+++ ClienteServidor-U3/ClienteWebSocket/Cliente.cs
@@ -0,0 +1,71 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace ClienteWebSocket
+{
+   public class Cliente: INotifyPropertyChanged
+    {
+        ClientWebSocket client = new ClientWebSocket();
+        public string Texto { get; set; }
+        public ICommand  EnviarCommand { get; set; }
+        Dispatcher dispatcher;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Cliente()
+        {
+            dispatcher = Dispatcher.CurrentDispatcher;
+            EnviarCommand = new RelayCommand(EnviarTexto);
+        }
+
+        private void EnviarTexto()
+        {
+            Enviar(Texto);
+            Texto = "";
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Texto"));
+        }
+
+        public async Task<bool> Conectar()
+        {
+           await client.ConnectAsync(new Uri("ws://localhost:1000/unidad3"), CancellationToken.None);
+            if(client.State==WebSocketState.Open)
+            {
+                Recibir();
+                return true;
+            }
+            return false;
+        }
+
+        public async void Enviar(string mensaje)
+        {
+            if(client.State==WebSocketState.Open)
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(mensaje);
+                await client.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+            }           
+        
+        }
+
+        public async void Recibir()
+        {
+            if(client.State==WebSocketState.Open)
+            {
+                byte[] buffer = new byte[1024];
+                var result= await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                string mensaje = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                MessageBox.Show(mensaje);
+                Recibir();
+            }
+        }
+    }
+}
---
+++ ClienteServidor-U3/Practica_PiedraPapelTijera/Juego.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Practica_PiedraPapelTijera
+{
+    public enum Opcion { Piedra, Papel, Tijeras}
+    public class Juego: INotifyPropertyChanged
+
+    {
+        public string Jugador1 { get; set; }
+        public string Jugador2 { get; set; }
+
+        public byte PuntosJugador1 { get; set; }
+        public byte PuntosJugador2 { get; set; }
+
+        public Opcion? SeleccionJugador1 { get; set; }
+        public Opcion? SeleccionJugador2 { get; set; }
+        public string Mensaje { get; set; }
+        public ICommand SeleccionarOpcionCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        HttpListener servidor;
+        ClientWebSocket cliente;
+        Dispatcher currentDispatcher;
+
+        public Juego()
+        {
+            currentDispatcher = Dispatcher.CurrentDispatcher;
+        }
+
+        public void CrearPartida(string nombre)
+        {
+            servidor = new HttpListener();
+            servidor.Prefixes.Add("http://*:1000/ppt/");
+            servidor.Start();
+            servidor.BeginGetContext(OnContext, null);
+            Jugador1 = nombre;
+            Mensaje = "Esperando que se conecte un adversario";
+        }
+
+        private void OnContext(IAsyncResult ar)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Actualizar(string propertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
